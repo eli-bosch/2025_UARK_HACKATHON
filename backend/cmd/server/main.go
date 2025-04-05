ea90dbe711da24f36596cb9148047c45abc32aa7
Eli Bosch <eli.bosch04@gmail.com>
2025-04-04 21:38:07 -0500

    Created MongoDB & Connection
    Need to update uri for MongoDB in main.go, once website is hosted on github.

--- backend/cmd/server/main.go
+++ backend/cmd/server/main.go
@@ -5,14 +5,16 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/eli-bosch/2025_UARK_HACKATHON/internal/db"
 	"github.com/eli-bosch/2025_UARK_HACKATHON/internal/routes"
 	"github.com/gorilla/mux"
 )
 
 func main() {
+	db.InitMongoDB("mongodb://localhost:27017") //Need to change once website is hosted
 	r := mux.NewRouter()
 	routes.TestRoutes(r)
-	
+
 	fmt.Println("server listening...")
 	log.Fatal(http.ListenAndServe("localhost:9010", r))
 }
--- backend/internal/db/db.go
+++ backend/internal/db/db.go
@@ -1,1 +1,25 @@
 package db
+
+import (
+	"context"
+	"log"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var Client *mongo.Client
+
+func InitMongoDB(uri string) {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(uri))
+	if err != nil {
+		log.Fatalf("MongoDB connection error: %v", err)
+	}
+
+	Client = client
+	log.Println("Connected to MongoDB!")
+}
